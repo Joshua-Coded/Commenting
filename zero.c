a710847d4ea81afe28b5037879d1f01a30077e06
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-26 00:48:32 +0100

    c program

---
+++ zero.c
@@ -0,0 +1,16 @@
+/** a program that returns zero **/
+
+#include <stdio.h>
+
+float answer;
+
+int main()
+
+{
+
+  answer = 1/3;
+  printf("the value of 1/3 is %f\n", answer);
+  return (0);
+
+
+}
